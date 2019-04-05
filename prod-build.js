f3e5b189051fa3bc036070f8b4ff70438a67ca02
Halli jaouad <hallijaouad@yahoo.fr>
2019-04-05 21:48:50 +0200

    Ajout script bluid prod and concat

---
+++ prod-build.js
@@ -0,0 +1,18 @@
+const fs = require('fs-extra');
+const concat = require('concat');
+
+(async function build() {
+  const files = [
+    './dist/iPaieManagerFrontEnd/runtime.js',
+    './dist/iPaieManagerFrontEnd/polyfills.js',
+    './dist/iPaieManagerFrontEnd/main.js'
+  ];
+
+  await fs.ensureDir('D:/j2ee/iPaieManager/iPaieManagerApi/src/main/resources/public');
+  await concat(files, 'D:/j2ee/iPaieManager/iPaieManagerApi/src/main/resources/static/app_angular.js');
+  await fs.copyFile(
+    './dist/iPaieManagerFrontEnd/styles.css',
+    'D:/j2ee/iPaieManager/iPaieManagerApi/src/main/resources/static/styles.css'
+  );
+  console.log('process concat cool for o2s poc angular.>> merci google');
+})();
